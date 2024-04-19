b365ab6971d3d7af1635f2ee750af888f30e23b5
Volodymyr Nerovnia <nerv@i.ua>
2024-04-20 01:45:56 +0300

    Enhanced style of AuthPage component

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -4,24 +4,26 @@ import "./AuthPage.css";
 export default function AuthPage() {
   return (
     <form action="#" className="login-form">
-      <div className="login-form__input">
-        <label htmlFor="">
-          <span>*</span>Username:
-        </label>
-        <input type="text" name="" id="" />
-      </div>
-      <div className="login-form__input">
-        <label htmlFor="">
-          <span>*</span>Password:
-        </label>
-        <input type="text" name="" id="" />
-      </div>
-      <div className="login-form__checkbox">
-        <input type="checkbox" id="" value="" />
-        <span className="checkbox-span">Remember me</span>
-      </div>
-      <div>
-        <Button />
+      <div className="login-form--container">
+        <div className="login-form__input">
+          <label htmlFor="">
+            <span>*</span>Username:
+          </label>
+          <input type="text" name="" id="" />
+        </div>
+        <div className="login-form__input">
+          <label htmlFor="">
+            <span>*</span>Password:
+          </label>
+          <input type="text" name="" id="" />
+        </div>
+        <div className="login-form__checkbox">
+          <input type="checkbox" id="" value="" />
+          <span className="checkbox-span">Remember me</span>
+        </div>
+        <div>
+          <Button />
+        </div>
       </div>
     </form>
   );
