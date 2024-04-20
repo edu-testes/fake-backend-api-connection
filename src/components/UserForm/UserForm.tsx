29e960ccbfb8fd3c81618f52459f9195a9509878
Volodymyr Nerovnia <nerv@i.ua>
2024-04-20 17:36:04 +0300

    Set useState for data in the UserForm component and added handleFormSubmit function for send data to the server

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,28 +1,53 @@
+import {useState, ChangeEvent, FormEvent} from "react";
 import Button from "../Button/Button.tsx";
 import "./UserForm.css";
 
-export default function UserForm() {
+type UserFormProps = {
+  reqType: string,
+}
+
+export default function UserForm(props: UserFormProps) {
+  const [formDataObject, setFormDataObject] = useState({
+    login: "",
+    password: "",
+    "remember-me": false
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value, type, checked } = e.target;
+    const updatedValue = type === 'checkbox' ? checked : value;
+    setFormDataObject({
+      ...formDataObject,
+      [name]: updatedValue,
+    });
+  };
+
+  function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(formDataObject);
+  }
+
   return (
-    <form action="#" className="login-form">
+    <form action="#" className="login-form" onSubmit={handleFormSubmit}>
       <div className="login-form--container">
         <div className="login-form__input">
           <label htmlFor="">
             <span>*</span>Username:
           </label>
-          <input type="text" name="" id="" />
+          <input type="text" name="login" id="" onChange={handleChange}/>
         </div>
         <div className="login-form__input">
           <label htmlFor="">
             <span>*</span>Password:
           </label>
-          <input type="text" name="" id="" />
+          <input type="text" name="password" id=""  onChange={handleChange}/>
         </div>
         <div className="login-form__checkbox">
-          <input type="checkbox" id="" value="" />
+          <input type="checkbox" id="" name="remember-me" value="" onChange={handleChange}/>
           <span className="checkbox-span">Remember me</span>
         </div>
         <div>
-          <Button />
+          <Button typeButton="submit"/>
         </div>
       </div>
     </form>
