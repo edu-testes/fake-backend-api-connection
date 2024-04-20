dc2a3d36c8cb0d441a078f0a99c667d387dde40e
Volodymyr Nerovnia <nerv@i.ua>
2024-04-21 00:16:28 +0300

    Added sendData property to the components AuthPage and RegisterPage

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,10 +1,11 @@
 import UserForm  from "../components/UserForm/UserForm.tsx";
+import { authUser } from "../services/authorization.ts"
 
 export default function AuthPage() {
   return (
     <>
       <p>Auth page</p>
-      <UserForm reqType={"auth"}/>
+      <UserForm reqType={"auth"} sendData={authUser}/>
     </>
   ) 
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,10 +1,11 @@
 import UserForm  from "../components/UserForm/UserForm.tsx";
+import { registerUser } from "../services/authorization.ts"
 
 export default function RegisterPage() {
   return (
     <>
       <p>Register page</p>
-      <UserForm reqType={"register"}/>
+      <UserForm reqType={"register"} sendData={registerUser}/>
     </>
   ) 
 }
