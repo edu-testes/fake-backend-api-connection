f01699f77552bd2e6c3f941c041ba0a3706d6c55
Volodymyr Nerovnia <nerv@i.ua>
2024-04-22 21:22:57 +0300

    Changed vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /*
+  build: {
+    sourcemap: false,
+  },
+  server: {
+    hmr: {
+      clientPort: 3000, // Ensure this port is accessible in your environment
+      overlay: false, // Disable the web server overlay
+      logLevel: 'error', // Change the log level to display only errors
+    },
+  },*/
 })
