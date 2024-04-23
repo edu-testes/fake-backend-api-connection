2bcc91e360356731dcfdf7f1b55ab566765fad08
Volodymyr Nerovnia <nerv@i.ua>
2024-04-23 11:20:02 +0300

    Stylized Users component

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -3,7 +3,6 @@ import { reqGetUsers }  from "../api/requests";
 export async function getUsers() {
   try {
     const res = await reqGetUsers();
-    console.log(res);
     return res;
   } catch(err) {
     console.log(err)
