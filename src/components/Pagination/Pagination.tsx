b618975e168cb146a94f53749519d313aaad4ebc
Volodymyr Nerovnia <nerv@i.ua>
2024-04-23 14:39:09 +0300

    Created pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,25 @@
+import "./Pagination.css";
+
+export default function Pagination() {
+  return (
+    <ul className="pagination">
+      <li><a href=""></a>&#x25C0;</li>
+      <li><a href=""></a>-500</li>
+      <li><a href=""></a>-100</li>
+      <li><a href=""></a>-50</li>
+      <li><a href=""></a>-10</li>
+      <li><a href=""></a>-5</li>
+      <li><a href=""></a>1</li>
+      <li><a href=""></a>2</li>
+      <li><a href=""></a>3</li>
+      <li><a href=""></a>4</li>
+      <li><a href=""></a>5</li>
+      <li><a href=""></a>+5</li>
+      <li><a href=""></a>+10</li>
+      <li><a href=""></a>+50</li>
+      <li><a href=""></a>+100</li>
+      <li><a href=""></a>+500</li>
+      <li><a href=""></a>&#x25B6;</li>
+    </ul>
+  );
+}
