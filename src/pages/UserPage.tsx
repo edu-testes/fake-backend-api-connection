faf96912f50f71ddbe9067c5029608e2fe51e945
Volodymyr Nerovnia <nerv@i.ua>
2024-04-23 10:44:43 +0300

    Enhanced UserPage component

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,9 +1,14 @@
-import Users from "../components/Users";
-import 
+import { useState, useEffect } from "react";
+import { getUsers } from "../services/getData"
+import Users from "../components/Users/Users";
 
 export default function UserPage() {
-
+  const [ users, setUsers ] = useState([]);
+  useEffect(() => {
+    getUsers().then((users) => setUsers(users)).catch((error) => console.log(error));
+  }, [])
   return (
-    <Users users={}/>
-  )
+     <Users users={users} />
+)
 }
+
